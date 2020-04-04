229b249eaee98312854108d479b83c59794722c3
kevinchrist20 <codemonk227@protonmail.com>
2020-04-04 16:06:28 +0300

    Implemented custom interceptor and logging interceptor. Added an okhttp client to retrofit

--- app/src/main/java/com/axionteq/newsapp/retrofit/RetrofitClient.java
+++ app/src/main/java/com/axionteq/newsapp/retrofit/RetrofitClient.java
@@ -2,6 +2,8 @@ package com.axionteq.newsapp.retrofit;
 
 import com.axionteq.newsapp.BuildConfig;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,13 +11,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     private static final String BASE_URL = BuildConfig.BASE_URL;
-    private static final String API_KEY = BuildConfig.API_KEY;
+
+    private static OkHttpClient client = new OkHttpClient().newBuilder()
+            .addNetworkInterceptor(new CustomInterceptor())
+            .addInterceptor(new HttpLoggingInterceptor())
+            .build();
 
     private static Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
                 .build();
     }
 
